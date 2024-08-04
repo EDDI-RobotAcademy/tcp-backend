b4988ec661403cea90a01a42c34ca16908fc224f
JeongAram <arongj6317@gmail.com>
2024-08-04 19:31:35 +0900

    feat: [정아람] 사용자로부터 요청받은 게시글 수정 [TCPD-5]

--- tcp_backend/tcp_django/board/urls.py
+++ tcp_backend/tcp_django/board/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path("", include(router.urls)),
     path("list/", BoardView.as_view({"get": "list"}), name="board-list"),
     path("register", BoardView.as_view({"post": "create"}), name="board-register"),
-    path('read/<int:pk>', BoardView.as_view({'get': 'read'}), name='board-read'),
+    path("read/<int:pk>", BoardView.as_view({"get": "read"}), name="board-read"),
     path("delete/<int:pk>", BoardView.as_view({"delete": "removeBoard"}), name="board-remove"),
+    path("modify/<int:pk>", BoardView.as_view({"put": "modifyBoard"}), name="board-modify"),
 ]
