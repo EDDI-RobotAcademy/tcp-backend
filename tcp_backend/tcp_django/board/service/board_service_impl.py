eb3dba184564bb9e2f60108efc648d533add49b3
JeongAram <arongj6317@gmail.com>
2024-08-04 17:30:33 +0900

    feat: [정아람] 사용자로부터 게시글 상세 정보를 요청 받아 출력합니다. [TCPD-3]

--- tcp_backend/tcp_django/board/service/board_service_impl.py
+++ tcp_backend/tcp_django/board/service/board_service_impl.py
@@ -24,3 +24,6 @@ class BoardServiceImpl(BoardService):
 
     def createBoard(self, boardData):
         return self.__boardRepository.create(boardData)
+
+    def readBoard(self, boardId):
+        return self.__boardRepository.findByBoardId(boardId)
